2a2dc551e7fa89f2d27faf983db4f5f0f66ac2d0
Ankit Kumar Yadav <itsankit9341@gmail.com>
2025-07-04 02:05:00 +0530

    .

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
+import "./FoodDisplay.css"
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
